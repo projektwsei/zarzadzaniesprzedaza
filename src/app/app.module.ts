bd27aa71c4ce3a1daa65250c0ecb971e7aafc55e
Katerina <ekatrinadotsenko@gmail.com>
2018-05-23 23:47:19 +0200

    widok faktury, oddzielnie css do login i registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,30 @@ import { AuthGuard } from './auth/guard.auth';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HeaderComponent } from './header/header.component';
+import { InvoiceListComponent } from './invoice/invoice-list/invoice-list.component';
+import { InvoiceAddComponent } from './invoice/invoice-add/invoice-add.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    HeaderComponent
+    HeaderComponent,
+    InvoiceListComponent,
+    InvoiceAddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [AuthGuard],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
+import { InvoiceAddComponent } from './invoice/invoice-add/invoice-add.component';
 
 
 const appRoutes: Routes = [
@@ -14,6 +15,7 @@ const appRoutes: Routes = [
         path: 'register',
         loadChildren: './auth/registration/registration.module#RegistrationModule'
     },
+    { path: 'invoice-add', component: InvoiceAddComponent },
     // otherwise redirect to HomePageComponent
     { path: '**', redirectTo: '' }
 ];
